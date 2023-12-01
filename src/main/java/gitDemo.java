6992a1c5e52ce9024bcaa1b4123aac5667756da7
Ouyang <Ouyang@nclg.com>
2023-12-01 14:49:05 +0800

    second commit

--- src/main/java/gitDemo.java
+++ src/main/java/gitDemo.java
@@ -1,9 +1,10 @@
 /**
  * @author  ouyang
- * @date 2023/12/1 - 14:31
+ * @date 2023/12/1 - 14:48
  */
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("first git-demo");
+        System.out.println("first git-demo2");
     }
 }
